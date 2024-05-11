70f0c1b16c3deef4e6dfae2fec9499c445ea29d4
xhc <2923637940@qq.com>
2024-05-11 09:54:15 +0800

    后端仓库搭建

---
+++ src/main/java/com/example/SmartEduX/SmartEduXApplication.java
@@ -0,0 +1,13 @@
+package com.example.SmartEduX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartEduXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartEduXApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SmartEduX/SmartEduXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SmartEduX;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartEduXApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
