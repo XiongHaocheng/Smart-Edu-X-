3046d0f2340cc1f0f504597d5abcaae7a90fdf7e
xhc <2923637940@qq.com>
2024-05-22 09:45:59 +0800

    录播课完成

--- src/main/java/com/example/SmartEduX/Controller/CommentController.java
+++ src/main/java/com/example/SmartEduX/Controller/CommentController.java
@@ -26,12 +26,37 @@ public class CommentController {
     @Autowired
     @Resource
     private UserMapper userMapper;
-    @ApiOperation("获取评论信息")
+    @ApiOperation("获取大课程评论信息")
     @CrossOrigin
-    @GetMapping(value = "/commentinfo")
-    public Result<List<Map<String, Object>>> getCommentinfo(@RequestParam Integer courseId) {
+    @GetMapping(value = "/coursecommentinfo")
+    public Result<List<Map<String, Object>>> getCourseCommentinfo(@RequestParam Integer courseId) {
         QueryWrapper<Comment> queryWrapper = new QueryWrapper<>();
         queryWrapper.eq("courseid", courseId);
+        List<Comment> comments = commentMapper.selectList(queryWrapper);
+        // 检查是否有相关记录
+        if (comments == null || comments.isEmpty()) {
+            return Result.error("-1","找不到评论信息");
+        }
+        // 构造返回的评论列表
+        List<Map<String, Object>> result = new ArrayList<>();
+        for (Comment comment : comments) {
+            Map<String, Object> commentMap = new HashMap<>();
+            commentMap.put("avatar", comment.getAvatar());
+            commentMap.put("name", comment.getName());
+            commentMap.put("commenttime", comment.getCommenttime());
+            commentMap.put("commentcontent", comment.getCommentcontent());
+            result.add(commentMap);
+        }
+
+        return Result.success(result,"成功");
+    }
+
+    @ApiOperation("获取单节录播课程评论信息")
+    @CrossOrigin
+    @GetMapping(value = "/videocommentinfo")
+    public Result<List<Map<String, Object>>> getVideoCourseCommentinfo(@RequestParam Integer videoCourseID) {
+        QueryWrapper<Comment> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("videocourseid", videoCourseID);
         List<Comment> comments = commentMapper.selectList(queryWrapper);
         // 检查是否有相关记录
         if (comments == null || comments.isEmpty()) {
@@ -54,8 +79,8 @@ public class CommentController {
 
     @ApiOperation("新增大课程评论信息")
     @CrossOrigin
-    @PostMapping ("/addcomment")
-    public Result<List<Map<String, Object>>> getCommentinfo(@RequestBody Comment request) {
+    @PostMapping ("/addcoursecomment")
+    public Result<List<Map<String, Object>>> addCourseComment(@RequestBody Comment request) {
         String commentContent = request.getCommentcontent();
         Integer userId = request.getUserid();
         Integer courseId = request.getCourseid();
@@ -81,6 +106,41 @@ public class CommentController {
         comment.setCommentcontent(commentContent);
         comment.setUserid(userId);
         comment.setCourseid(courseId);
+
+        commentMapper.insert(comment);
+
+        return Result.success("评论成功");
+    }
+
+    @ApiOperation("新增单节录播课程评论信息")
+    @CrossOrigin
+    @PostMapping ("/addvideocoursecomment")
+    public Result<List<Map<String, Object>>> addVideoCourseComment(@RequestBody Comment request) {
+        String commentContent = request.getCommentcontent();
+        Integer userId = request.getUserid();
+        Integer videocourseId = request.getVideocourseid();
+
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("userid", userId);
+        List<User> userList = userMapper.selectList(queryWrapper);
+
+        User user = userList.get(0); // 只有一个用户匹配
+
+        // 获取当前评论的时间
+        Date currentTime = new Date();
+        // 创建SimpleDateFormat对象，指定日期格式为 "yyyy-MM-dd"
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        // 格式化日期为字符串
+        String commentTime = sdf.format(currentTime);
+
+        // 插入评论数据到数据库中
+        Comment comment = new Comment();
+        comment.setAvatar(user.getUseravatar());
+        comment.setName(user.getUsername());
+        comment.setCommenttime(commentTime);
+        comment.setCommentcontent(commentContent);
+        comment.setUserid(userId);
+        comment.setVideocourseid(videocourseId);
 
         commentMapper.insert(comment);
 
--- src/main/java/com/example/SmartEduX/Controller/VideoCourseController.java
+++ src/main/java/com/example/SmartEduX/Controller/VideoCourseController.java
@@ -2,6 +2,7 @@ package com.example.SmartEduX.Controller;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.example.SmartEduX.Mapper.VideoCourseMapper;
 import com.example.SmartEduX.common.Result;
+import com.example.SmartEduX.entity.User;
 import com.example.SmartEduX.entity.VideoCourse;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -11,6 +12,8 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.*;
+import java.util.stream.Collectors;
+
 @Mapper
 @Api(tags = "API接口")
 @RestController
@@ -20,6 +23,7 @@ public class VideoCourseController {
     @Autowired
     @Resource
     private VideoCourseMapper videoCourseMapper;
+
     @ApiOperation("获取录播课程目录信息")
     @CrossOrigin
     @GetMapping("/videocatagorycourseinfo")
@@ -32,7 +36,7 @@ public class VideoCourseController {
 
             // 检查是否有相关记录
             if (videoCourses == null || videoCourses.isEmpty()) {
-                return Result.error("-1","找不到小章节信息");
+                return Result.error("-1", "找不到小章节信息");
             }
 
             // 将结果组装成 Map 对象
@@ -46,11 +50,59 @@ public class VideoCourseController {
                 result.get(order).add(courseName);
             }
             // 返回结果
-            return Result.success(result,"成功");
+            return Result.success(result, "成功");
         } catch (Exception e) {
             e.printStackTrace();
-            return Result.error("-1","找不到小章节信息");
+            return Result.error("-1", "找不到小章节信息");
         }
+    }
+
+    @ApiOperation("播放界面的小章节信息以及ID获取")
+    @CrossOrigin
+    @GetMapping("/videochaptersinfo")
+    public Result<Map<String, Object>> getVideoChapters(@RequestParam Integer courseId, @RequestParam String videoCourseName) {
+        QueryWrapper<VideoCourse> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("courseid", courseId).eq("videocoursename", videoCourseName);
+        List<VideoCourse> videoCourses = videoCourseMapper.selectList(queryWrapper);
+
+        QueryWrapper<VideoCourse> queryWrapper1 = new QueryWrapper<>();
+        queryWrapper1.eq("videocoursename", videoCourseName);
+        List<VideoCourse> videocourse = videoCourseMapper.selectList(queryWrapper1);
+
+        // 获取该小章节的videoOrder
+        Integer videoOrder = videoCourses.get(0).getVideoorder();
+        // 获取该小章节的ID
+        Integer videoCourseID = videocourse.get(0).getVideocourseid();
+
+        // 根据videoOrder和courseId查找数据库中所有的videoCourseName
+        QueryWrapper<VideoCourse> queryWrapper2 = new QueryWrapper<>();
+        queryWrapper2.eq("courseid", courseId).eq("videoorder", videoOrder);
+        List<VideoCourse> allVideoCourses = videoCourseMapper.selectList(queryWrapper2);
+
+        if (allVideoCourses.isEmpty()) {
+            return Result.error("-1", "未找到数据");
+        }
+
+        List<String> videoCourseNames = new ArrayList<>();
+        for (VideoCourse course : allVideoCourses) {
+            videoCourseNames.add(course.getVideocoursename());
+        }
+
+        // 创建返回结果
+        Map<String, Object> resultData = new HashMap<>();
+        resultData.put("videoCourseID", videoCourseID);
+        resultData.put("videoCourseNames", videoCourseNames);
+
+        return Result.success(resultData, "成功");
+    }
+
+    @ApiOperation("获取播放视频链接")
+    @CrossOrigin
+    @GetMapping("/videoplaylinkinfo")
+    public Result<String> getVideoPlayLink(@RequestParam Integer videoCourseID) {
+        VideoCourse videoCourse = videoCourseMapper.selectById(videoCourseID);
+        String result = videoCourse.getPlaylink();
+        return Result.success(result,"成功");
     }
 }
 
