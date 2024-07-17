627e75c0db29075216a4c33649585a410a7b354a
xhc <2923637940@qq.com>
2024-07-17 11:33:49 +0800

    文本合规检测

--- src/main/java/com/example/SmartEduX/xunfeiAPI/TextLegal/Constants.java
+++ src/main/java/com/example/SmartEduX/xunfeiAPI/TextLegal/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
     /**
      * 控制台获取以下信息 https://console.xfyun.cn/services/text_audit
      */
-    public static final String APPID = "b48d3d11";
-    public static final String APISecret = "ZjYzNTBmZGM4OGI2ZDFhNDdjNTE1N2U5";
-    public static final String APIKey = "1de36e6a0bc7e956fc706471bfc060e0";
+    public static final String APPID = "c54fc911";
+    public static final String APISecret = "YmI3YzI0Mzg2MmY0NWQzYmExNDYzM2Jj";
+    public static final String APIKey = "21ad048e11ea0df413094aae1567bb4e";
 }
